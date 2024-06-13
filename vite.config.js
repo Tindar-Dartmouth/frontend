e9752dbb54be674216386209cdceabdc00e33bdb
Caleb Ash <124649246+calebashw@users.noreply.github.com>
2024-06-12 23:29:52 -0400

    Package and related changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(), // Add this line to include the React plugin
     eslint(),
   ],
   css: {
