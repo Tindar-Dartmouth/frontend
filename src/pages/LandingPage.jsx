5bc10c58570f4b49ca3b71a9cbd7d3a5d0f4ef53
Caleb Ash <124649246+calebashw@users.noreply.github.com>
2024-09-08 22:26:43 -0400

    Some styling updates on landing page (also did this branch get created/is git working)

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Stack from '@mui/material/Stack';
-import { Button, Flex } from 'antd';
+import { Flex } from 'antd';
 // import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import '../style/LandingPageStyle.css';
@@ -19,12 +19,14 @@ function LandingPage() {
   return (
     <div className="landing-page-container">
       <div className="landing-page-header">
-        <h1>Welcome to Tindar</h1>
+        <h1>Tindar</h1>
+        <br />
+        <h2>Strictly Business</h2>
       </div>
       <div className="register-login-buttons">
         <Flex gap="large" wrap>
-          <Button onClick={handleClickLogin} type="primary">Login</Button>
-          <Button onClick={handleClickRegister}>Register</Button>
+          <button className="login-button" onClick={handleClickLogin} type="button">Login</button>
+          <button className="register-button" type="button" onClick={handleClickRegister}>Register</button>
         </Flex>
       </div>
       {/*
